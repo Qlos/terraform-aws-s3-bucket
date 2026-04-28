2dac8593757676013fa475ed0c135b821a855519
Paweł Owczarek <pawel.owczarek@qlos.com>
2026-04-28 15:04:32 +0200

    add cors inputs

--- variables.tf
+++ variables.tf
@@ -177,3 +177,15 @@ variable "tags" {
   default     = {}
   description = "Map of tags to assign to bucket."
 }
+
+variable "cors_rules" {
+  type = list(object({
+    allowed_headers = list(string)
+    allowed_methods = list(string)
+    allowed_origins = list(string)
+    expose_headers  = list(string)
+    max_age_seconds = number
+  }))
+  default     = []
+  description = "List of CORS rules for the bucket."
+}
